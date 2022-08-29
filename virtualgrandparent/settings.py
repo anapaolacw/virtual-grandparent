ba95c6507803bdc921764f44bfff47ca6213f773
anapaolacw <apcw96@gmail.com>
2022-08-29 23:12:40 +0100

    core(base): add requirements

--- virtualgrandparent/settings.py
+++ virtualgrandparent/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,3 +150,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#Activate Django-Heroku
+django_on_heroku.settings(locals())
